fb797f5c3eb720dc02476324822ccbb60f7c8a1c
AndresQ <tulsidas@gmail.com>
2020-07-02 12:45:41 -0300

    MEC

---
+++ src/main/java/ar/com/jengibre/filtrodla/MEC.java
@@ -0,0 +1,152 @@
+package ar.com.jengibre.filtrodla;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Iterator;
+import java.util.Optional;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.FillPatternType;
+import org.apache.poi.ss.usermodel.IndexedColors;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFCellStyle;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import com.google.common.collect.ImmutableList;
+
+public class MEC {
+
+  private static final NumberFormat NF = new DecimalFormat("0.#");
+
+  public static void run(String filtro, String plani) throws Exception {
+    XSSFWorkbook peliculas = new XSSFWorkbook(new FileInputStream(plani));
+
+    XSSFWorkbook out = new XSSFWorkbook();
+    XSSFSheet outSheet = out.createSheet();
+
+    try (XSSFWorkbook reprocesamiento = new XSSFWorkbook(new FileInputStream(filtro))) {
+      XSSFSheet sheet = reprocesamiento.getSheet("reporte 30-6");
+      Iterator<Row> iterator = sheet.iterator();
+      iterator.next(); // salteo la primera
+
+      while (iterator.hasNext()) {
+        buscarPeli(peliculas, (XSSFRow) iterator.next(), outSheet);
+      }
+
+      System.out.println("\n\nListo");
+    }
+
+    peliculas.close();
+
+    out.write(new FileOutputStream("out.xlsx"));
+    out.close();
+  }
+
+  private static void buscarPeli(final XSSFWorkbook workbook, final XSSFRow rowBusqueda,
+      final XSSFSheet outSheet) {
+
+    ImmutableList<Sheet> reverseSheets = ImmutableList.copyOf(workbook).reverse();
+    cellValue(rowBusqueda.getCell(2)).ifPresent(titulo -> {
+
+      sheet: for (Sheet sheet : reverseSheets) {
+        for (Row _row : sheet) {
+          XSSFRow row = (XSSFRow) _row;
+          Optional<String> _filmTitle = cellValue(row.getCell(2)); // columna C - film title
+
+          if (_filmTitle.isPresent()) {
+            String filmTitle = _filmTitle.get();
+            if (filmTitle.equalsIgnoreCase(titulo)) {
+              String sheetName = sheet.getSheetName();
+
+              System.out.println(
+                  titulo + " encontrada en " + sheetName);
+
+              XSSFRow newRow = outSheet.createRow(rowBusqueda.getRowNum());
+
+              // Loop through source columns to add to new row
+              for (int i = 0; i < row.getLastCellNum(); i++) {
+                // Grab a copy of the old/new cell
+                XSSFCell oldCell = row.getCell(i);
+                XSSFCell newCell = newRow.createCell(i);
+
+                // If the old cell is null jump to next cell
+                if (oldCell == null) {
+                  newCell = null;
+                  continue;
+                }
+
+                XSSFCellStyle style = outSheet.getWorkbook().createCellStyle();
+                if (oldCell.getCellStyle().getFillForegroundXSSFColor() == null) {
+                  style.setFillForegroundColor(IndexedColors.WHITE.index);
+                }
+                else {
+                  style.setFillForegroundColor(oldCell.getCellStyle().getFillForegroundXSSFColor());
+                }
+                style.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+                newCell.setCellStyle(style);
+
+                // Set the cell data value
+                switch (oldCell.getCellType()) {
+                  case BLANK:
+                    newCell.setBlank();
+                    break;
+                  case BOOLEAN:
+                    newCell.setCellValue(oldCell.getBooleanCellValue());
+                    break;
+                  case ERROR:
+                    newCell.setCellErrorValue(oldCell.getErrorCellValue());
+                    break;
+                  case FORMULA:
+                    newCell.setCellFormula(oldCell.getCellFormula());
+                    break;
+                  case NUMERIC:
+                    newCell.setCellValue(oldCell.getNumericCellValue());
+                    break;
+                  case STRING:
+                    newCell.setCellValue(oldCell.getStringCellValue());
+                    break;
+                  case _NONE:
+                    break;
+                  default:
+                    break;
+                }
+              }
+
+              // en la N va el nombre del sheet
+              newRow.createCell(13, CellType.STRING).setCellValue(sheetName);
+
+              // en la O Comentarios Laboratorio
+              newRow.createCell(14, CellType.STRING)
+                  .setCellValue(rowBusqueda.getCell(3).getStringCellValue());
+
+              break sheet;
+            }
+          }
+        }
+      }
+    });
+  }
+
+  private static Optional<String> cellValue(Cell cell) {
+    if (cell == null) {
+      return Optional.empty();
+    }
+    else {
+      switch (cell.getCellType()) {
+        case NUMERIC:
+          return Optional.of(NF.format(cell.getNumericCellValue()));
+        case STRING:
+          return Optional.of(cell.getStringCellValue().trim().replaceAll("\\n", " "));
+        default:
+          return Optional.empty();
+      }
+    }
+  }
+}
---
+++ src/main/java/ar/com/jengibre/filtrodla/SuperFlash.java
@@ -0,0 +1,151 @@
+package ar.com.jengibre.filtrodla;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+import java.util.TreeSet;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import com.google.common.base.Charsets;
+import com.google.common.base.Joiner;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
+import com.google.common.io.Files;
+
+public class SuperFlash {
+
+  private static final char DELIM = ';';
+
+  public static void run(String filtro, String planilla) throws Exception {
+    TreeSet<String> titulosMa = titulos(filtro, "ma");
+    TreeSet<String> titulosDVB = titulos(filtro, "dvb");
+
+    try (XSSFWorkbook workbook = new XSSFWorkbook(new FileInputStream(planilla))) {
+      try (BufferedWriter writer = Files.newWriter(new File("ma.csv"), Charsets.UTF_8)) {
+        for (String titulo : titulosMa) {
+          writer.write("\"" + titulo + "\"");
+          writer.write(DELIM);
+          writer.write(buscarPeli(workbook, titulo, 5 /* ma */));
+          writer.write('\n');
+        }
+        writer.flush();
+      }
+
+      try (BufferedWriter writer = Files.newWriter(new File("dvb.csv"), Charsets.UTF_8)) {
+        for (String titulo : titulosDVB) {
+          writer.write("\"" + titulo + "\"");
+          writer.write(DELIM);
+          writer.write(buscarPeli(workbook, titulo, 6 /* dvb */));
+          writer.write('\n');
+        }
+        writer.flush();
+      }
+
+      System.out.println("Listo");
+    }
+  }
+
+  private static String buscarPeli(final XSSFWorkbook workbook, final String titulo, int columna) {
+    List<Peli> pelis = Lists.newArrayList();
+
+    ImmutableList<Sheet> reverseSheets = ImmutableList.copyOf(workbook).reverse();
+
+    for (Sheet sheet : reverseSheets) {
+      int colObservaciones = getColumn(sheet.getRow(0), "observaciones");
+      int colEntregado = getColumn(sheet.getRow(0), "entregado");
+
+      for (Row row : sheet) {
+        Cell filmTitleCell = row.getCell(2); // columna C - film title
+        Cell cell = row.getCell(columna); // columna ma/dbv
+
+        cellValue(filmTitleCell).ifPresent(filmTitle -> {
+          if (filmTitle.equalsIgnoreCase(titulo)) {
+            String sheetName = sheet.getSheetName();
+            int rowNum = row.getRowNum() + 1;
+            String media = cellValue(cell).orElse("");
+            String entregado = colEntregado == -1 ? "" : cellValue(row.getCell(colEntregado)).orElse("");
+            String observaciones = colObservaciones == -1 ? ""
+                : cellValue(row.getCell(colObservaciones)).orElse("");
+
+            pelis.add(new Peli(sheetName, rowNum, media, entregado, observaciones));
+          }
+        });
+      }
+    }
+
+    return Joiner.on(DELIM).join(pelis);
+  }
+
+  private static short getColumn(Row row, String name) {
+    if (row != null) {
+      for (short i = row.getFirstCellNum(); i < row.getLastCellNum(); i++) {
+        Cell cell = row.getCell(i);
+        if (cell != null && cell.getCellType() == CellType.STRING
+            && cell.getStringCellValue().trim().equalsIgnoreCase(name)) {
+          return i;
+        }
+      }
+    }
+
+    return -1;
+  }
+
+  private static Optional<String> cellValue(Cell cell) {
+    return (cell != null && cell.getCellType() == CellType.STRING)
+        ? Optional.of(cell.getStringCellValue().trim().replaceAll("\\n", " ")) : Optional.empty();
+  }
+
+  private static TreeSet<String> titulos(String archivo, String prefijo)
+      throws FileNotFoundException, IOException {
+    TreeSet<String> ret = Sets.newTreeSet();
+
+    try (XSSFWorkbook wb = new XSSFWorkbook(new FileInputStream(archivo))) {
+      XSSFSheet sheet = wb.getSheetAt(0); // unico sheet
+
+      for (int r = 2; r < sheet.getLastRowNum(); r++) {
+        XSSFRow row = sheet.getRow(r);
+
+        cellValue(row.getCell(0)).ifPresent(title -> {
+          cellValue(row.getCell(7)).ifPresent(media -> {
+            if (media.toLowerCase().startsWith(prefijo)) {
+              ret.add(title);
+            }
+          });
+        });
+      }
+    }
+
+    return ret;
+  }
+
+  static class Peli {
+    String sheet, ma, entregado, observaciones;
+
+    int row;
+
+    public Peli(String sheet, int row, String ma, String entregado, String observaciones) {
+      this.sheet = sheet;
+      this.row = row;
+      this.ma = ma;
+      this.entregado = entregado;
+      this.observaciones = observaciones;
+    }
+
+    @Override
+    public String toString() {
+      return sheet + DELIM + row + DELIM + ma + DELIM + entregado + DELIM + observaciones;
+    }
+  }
+}
